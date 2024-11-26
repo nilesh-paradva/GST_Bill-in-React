4aa868a50dbca58714c4bae3323cadcf7a885df6
nilesh7272 <nileshparadva97@gmail.com>
2024-11-26 16:55:07 +0530

    GST Bill

--- src/components/viewbill/ViewBill.jsx
+++ src/components/viewbill/ViewBill.jsx
@@ -17,7 +17,7 @@ const ViewBill = () => {
 
     return (
         <>
-            <section className="py-60 my-32">
+            <section className="py-60 my-24">
                 <Container>
                     <Row>
                         <div className="col-lg-12 flex flex-col items-center justify-center h-screen">
