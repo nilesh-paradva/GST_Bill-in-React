285bdcbb57e58a8d4a3d8491b44fb19cc000eab3
nilesh7272 <nileshparadva97@gmail.com>
2024-11-28 10:06:59 +0530

    GST Bill

--- src/components/textstyleform/TextStyleForm.jsx
+++ src/components/textstyleform/TextStyleForm.jsx
@@ -42,7 +42,7 @@ const TextStyleForm = () => {
 
   return (
     <>
-      <section className="py-24 my-8">
+      <section className="py-32 my-5">
         <Container>
           <Row>
             <div className="col-12 flex items-center justify-center h-screen">
